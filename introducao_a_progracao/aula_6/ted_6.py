cf0ff450adc44b3f3fbde65005bcbde4c6bca0c5
instrutor <hf3583904@gmail.com>
2024-09-30 20:51:37 -0300

    c

--- introducao_a_progracao/aula_6/ted_6.py
+++
@@ -1,6 +0,0 @@
-name = {}
-
-vingadores = nomes = ["Natasha Romanoff", "Tony Stark", "Peter Park", "Bruce Bener", "Thor", "Maria Hill", "Wanda Maximoff", "Gamora", "Okoye", "Nebulosa", "Mantis", "Shuri", "Stephen Strange"]
-
-for name in vingadores:
-    print(f'Olá, {name}, você está convidado(a) para a minha festa! boraa!')
